493c3893cb7e2cd6cfa745f96c9e40f1ee0277f7
Vladimir Feinberg <vyf@princeton.edu>
2015-10-16 21:07:50 -0400

    Preprocessing script edits

--- clean_csv.py
+++ clean_csv.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-
 """ Clean and merge csv files """
 
 from __future__ import division
 from os import listdir
+from os import getcwd
 import csv
 from datetime import datetime
 import tldextract
@@ -32,50 +31,29 @@ def check_row(row):
     return True
 
 
-def hash_string(s):
-    """ hash strings """
-
-    if s == '':
-        return 0
-    else:
-        return hash(s)
-
-
 def clean_row(row):
     """ Clean a row to [2, 27, 30, 31, 7, 17, 26, 32, 33, 34, 35, 37, 44, 58] """
 
-    row.extend([''] * (NUM_FIELDS - len(row)))
+    if len(row) < NUM_FIELDS:
+        row.extend('')
 
     new_row = []
 
-    sql_date = row[1]
-    event_code = row[26]
-    quad_class = row[29]
-    goldstein = row[30]
-
-    actor_1_name = hash_string(row[6])
-    actor_2_name = hash_string(row[16])
-
-    root_event = row[25]
-
-    num_mentions, num_sources, num_articles = row[31], row[32], row[33]
-    tone = row[34]
-    actor_1_geo_full_name = hash_string(row[36])
-    actor_2_geo_full_name = hash_string(row[43])
-
-    url = row[57]
 
     # format date
     unix_epoch = datetime(1970, 1, 1)
+    sql_date = row[1]
     parsed_sql_date = datetime(int(sql_date[:4]), int(sql_date[4:6]), int(sql_date[6:8]))
     days_since_epoch = (parsed_sql_date - unix_epoch).days
 
-    # format url
-    domain = hash_string(tldextract.extract(url).domain)
+    new_row.extend([days_since_epoch])
 
     # add other fields
-    new_row.extend([days_since_epoch, event_code, quad_class, goldstein, actor_1_name, actor_2_name, root_event, num_mentions, num_sources, num_articles, tone, actor_1_geo_full_name, actor_2_geo_full_name, domain])
+    new_row.extend([row[26], row[29], row[30], row[6], row[16], row[25], row[31], row[32], row[33], row[34], row[36], row[43]])
 
+    # format url
+    domain = tldextract.extract(row[57]).domain
+    new_row.extend([domain])
 
     return new_row
 
