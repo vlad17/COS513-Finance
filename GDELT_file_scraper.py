eaeb8c48878e16d03bbdb2f950117aa751f359e7
Daway Chou-Ren <dchouren@princeton.edu>
2015-11-05 01:16:17 -0500

    cosmetic

--- GDELT_file_scraper.py
+++ GDELT_file_scraper.py
@@ -75,27 +75,3 @@ elif year > 1978:
 
 
 
-
-
-# if int(year) > 2005: # year = 2006 or later
-#     for month in range(1, 13):
-
-#         if int(year) > 2013: # year = 2013 or later
-#             for day in range(1, 32):
-#                 filename = year + str(month).zfill(2) + str(day).zfill(2) + '.export.CSV.zip'
-#                 download_file(filename)
-
-#         elif int(year) == 2013:
-#             if month < 4:
-#                 filename = year + str(month).zfill(2) + '.zip'
-#             else:
-#                 filename = year + str(month).zfill(2) + str(day).zfill(2) + '.export.CSV.zip'
-#             download_file(filename)
-
-#         else:
-#             filename = year + str(month).zfill(2) + '.zip'
-#             download_file(filename)
-
-# else: # year = 2005 or earlier
-#     filename = year + '.zip'
-#     download_file(filename)
